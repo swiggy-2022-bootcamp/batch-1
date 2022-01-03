7ff2abf5250975be0758205dca339b395d71c945
aniket tatte <aniket.tatte@bfhl.in>
2022-01-03 21:16:12 +0530

    03-Jan Javascript task for Swiggy I++

---
+++ Aniket_Tatte[SWIGGYP-B1]/03_Jan/main.js
@@ -0,0 +1,7 @@
+function helloFunction(name) {
+    alert('Hope you are doing good and staying safe ' + name);
+} 
+
+function displayFunction2(data) {
+    console.log('Display Function 2 called with ' + data);
+}
