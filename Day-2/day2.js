4de4aa3986dc32a9735e68beb0504f9a19e597da
Jitesh Dhoot <bansal.jda@gmail.com>
2022-01-04 23:28:09 +0530

    Added Day-2 tasks

---
+++ Day-2/day2.js
@@ -0,0 +1,12 @@
+function changeFontSize(){
+    document.getElementById("text1").style.fontSize = "50px";
+}
+
+function changeDisplay(){
+    document.getElementById("text1").style.display = "none";
+}
+
+function changeVisibility(){
+    document.getElementById("text1").style.visibility = "hidden";
+}
+
