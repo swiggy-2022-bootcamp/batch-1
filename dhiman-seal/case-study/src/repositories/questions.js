eb15a3ac6659a36c710063e8ab3c4f96b7c63676
Dhiman Seal <furyx.ds@gmail.com>
2022-01-06 22:39:35 +0530

    /question endpoint CRUD

---
+++ dhiman-seal/case-study/src/repositories/questions.js
@@ -0,0 +1,119 @@
+import fs from 'fs/promises';
+
+import {QuestionData} from '../models/question_data.js';
+
+/**
+ * Path to the Users database.
+ */
+const questionsDBPath = './src/database/questions.json';
+
+/**
+ * Get the ID of the next question to be stored in the database.
+ * Implements autoincrement key strategy.
+ * @param {string} dbPathOverride - Override of the path to the
+ * database to be used. Defaults to the default defined path questionsDBPath.
+ * @return {Promise<number>} A promise that resolves to the next question ID.
+ */
+export const getNextQuestionID = async (dbPathOverride=questionsDBPath) => {
+  const questions = await getDatabase(dbPathOverride);
+  return Object.keys(questions).length + 1;
+};
+
+/**
+ * Create a new question in the database.
+ * @param {QuestionData} question - The input data of the
+ * question to be created.
+ * @param {string} dbPathOverride - Override of the path to the
+ * database to be used. Defaults to the default defined path questionsDBPath.
+ * @return {Promise<boolean>} A promise that resolves to whether the
+ * question was successfully created.
+ */
+export const createQuestion = async (
+    question,
+    dbPathOverride=questionsDBPath,
+) => {
+  const questions = await getDatabase(dbPathOverride);
+  if (question.title.length === 0) {
+    throw new Error('Question must have a title.');
+  }
+  if (questions[question.question_id]) {
+    throw new Error(`Question with ID ${question.question_id} already exists.`);
+  }
+  questions[question.question_id] = question.toJSON();
+  fs.writeFile(dbPathOverride, JSON.stringify(questions));
+  return true;
+};
+
+/**
+ * Fetch a question from the database.
+ * @param {string} questionID - The input data of the
+ * question to be created.
+ * @param {string} dbPathOverride - Override of the path to the
+ * database to be used. Defaults to the default defined path questionsDBPath.
+ * @return {Promise<QuestionData>} A promise that resolves to the question data.
+ */
+export const getQuestion = async (
+    questionID,
+    dbPathOverride=questionsDBPath,
+) => {
+  const questions = await getDatabase(dbPathOverride);
+  const qdJSON = questions[questionID];
+  return qdJSON ? QuestionData.fromJSON(qdJSON) : undefined;
+};
+
+/**
+ * Update a new question in the database.
+ * @param {string} questionID - The ID of the question to be updated.
+ * @param {QuestionData} question - The input data of the
+ * question to be updated. question.question_id is ignored
+ * @param {string} dbPathOverride - Override of the path to the
+ * database to be used. Defaults to the default defined path questionsDBPath.
+ * @return {Promise<boolean>} A promise that resolves to whether the
+ * question was successfully updated.
+ */
+export const updateQuestion = async (
+    questionID,
+    question,
+    dbPathOverride=questionsDBPath,
+) => {
+  const questions = await getDatabase(dbPathOverride);
+  if (question.title.length === 0) {
+    throw new Error('Question must have a title.');
+  }
+  if (!questions[questionID]) {
+    throw new Error(`Question with ID ${questionID} doesn't exist.`);
+  }
+  questions[questionID] = {
+    'question-ID': questionID,
+    ...question.toJSON(),
+  };
+  fs.writeFile(dbPathOverride, JSON.stringify(questions));
+  return true;
+};
+
+/**
+ * Deletes a question completely from the database if they exist.
+ * @param {string} questionID - ID of the user to be deleted.
+ * @param {string} dbPathOverride - Override of the path to the
+ * database to be used. Defaults to the default defined path usersDBPath.
+ * @return {Promise<void>} A promise that resolves when the user is deleted.
+ */
+export const deleteQuestion = async (
+    questionID,
+    dbPathOverride=usersDBPath,
+) => {
+  const questions = await getDatabase(dbPathOverride);
+  delete questions[questionID];
+  fs.writeFile(dbPathOverride, JSON.stringify(users));
+};
+
+
+/**
+ * Returns a database object as an asynchronous promise.
+ * @param {string} dbPath - The path of the database to be fetched.
+ * @param {string} dbPathOverride - Override of the path to the
+ * database to be used. Defaults to the default defined path questionsDBPath.
+ * @return {Promise<object>} A promise that resolves to the database json.
+ */
+const getDatabase = async (dbPath=questionsDBPath) =>
+  JSON.parse(await fs.readFile(dbPath, 'utf-8'));
