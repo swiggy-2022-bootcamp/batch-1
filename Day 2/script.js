375719f4662a9d487d6c4fcafaa109db6b0851dc
Aman Gupta <50948920+justAmanGupta@users.noreply.github.com>
2022-01-07 20:36:32 +0530

    Adding code

---
+++ Day 2/script.js
@@ -0,0 +1,2 @@
+document.getElementById('imgid').getAttributeNode('src').value = 'https://image.shutterstock.com/image-photo/small-light-clouds-blue-sky-260nw-444497878.jpg';
+document.getElementById('para').style.fontSize = '30px';
