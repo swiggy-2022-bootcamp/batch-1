39a0fcef701bde3348457cd822043751c0da1580
Chahat Bhatia <chahatbhatia2014@gmail.com>
2022-01-07 15:28:57 +0530

    Day2

---
+++ Day2/index.js
@@ -0,0 +1,9 @@
+alert("Hello");
+document.getElementById("first").innerHTML="<h4>Changed HTML</h4>";
+document.getElementById("second").style.fontSize="20px";
+document.getElementById("third").style.display="none";
+document.getElementById("fourth").style.visibility="hidden";
+document.getElementById("fifth").value="Changed Value";
+
+const fruits=["Banana", "Orange", "Apple", "Mango"];
+
