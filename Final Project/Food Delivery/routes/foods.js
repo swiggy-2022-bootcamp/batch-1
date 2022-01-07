73a3e8ba46e959b1b38d98843faf874b2837cdf8
XenoCod <adityakrsingh1999@gmail.com>
2022-01-07 23:22:30 +0530

    Implemented JWT and updated controllers

--- Final Project/Food Delivery/routes/foods.js
+++ Final Project/Food Delivery/routes/foods.js
@@ -2,7 +2,8 @@ const Food = require('../models/food');
 const express = require('express');
 const router = express.Router();
 
-//GET FOODS
+//GET ALL FOODS
+//API GET /api/food
 router.get('/', async (req, res) => {
     try {
         const foods = await Food.find();
@@ -10,9 +11,13 @@ router.get('/', async (req, res) => {
     } catch (error) {
         res.status(404).json({message: 'No foods not found'});
     }
-})
+});
+
+
+
 
 //GET ONE USER
+//API GET /api/users/:id
 router.get("/:id", getFood , async (req, res) => {
     if(res.food != null){
         res.status(200).json(res.food);
@@ -20,10 +25,13 @@ router.get("/:id", getFood , async (req, res) => {
     else{
         res.status(404).json({message: `Food with ${req.params.id} not found`});
     }
-})
+});
 
-//ADD USER
-router.post('/food', async (req, res) => {
+
+
+//ADD FOOD
+//API POST /api/food
+router.post('/', async (req, res) => {
 
    const food= new Food({
        foodId: req.body.foodId,
@@ -42,12 +50,77 @@ router.post('/food', async (req, res) => {
    
 });
 
+
+
+
+//DELETE FOOD
+//API POST /api/food
+router.delete('/:id', getFood, async (req, res) =>{
+    try{
+        await res.food.remove();
+        res.json({message:`Food with ${req.params.id} deleted successfully`})
+    }
+    catch(error) {
+        res.status(404).json({message: `Sorry user with ${req.params.id} not found`});
+    }
+});
+
+
+
+//UPDATE FOODS
+//API POST /api/food/:id
+router.put('/:id', getFood, async (req, res) => {
+
+    res.food.foodId= req.body.foodId;
+    res.food.foodName = req.body.foodName;
+    res.food.foodCost= req.body.foodCost;
+    res.food.foodType= req.body.foodType;
+
+    try{
+        const updatedFood = await res.food.save();
+        res.status(200).json(updatedFood);
+    }
+    catch(error) {
+        res.status(404).json({message: `Sorry food with ${req.params.id} not found`});
+    }
+});
+
+
+
+
+//PATCH FOOD (UPDATE ANY SINGLE ENTITY)
+//API POST /api/food/:id
+router.patch('/:id', getFood, async (req, res) => {
+    if(req.body.foodId != null){
+        res.food.foodId= req.body.foodId;
+    }
+    else if(req.body.foodName != null){
+        res.food.foodName = req.body.foodName;
+    }
+    else if(req.body.foodCost != null){
+        res.food.foodCost= req.body.foodCost;
+    }
+    else if(req.body.foodType != null){
+        res.food.foodType= req.body.foodType;
+    }
+    try{
+        const updatedFood = await res.food.save();
+        res.status(200).json(updatedFood);
+    }
+    catch(error) {
+        res.status(404).json({message: `Sorry food with ${req.params.id} not found`});
+    }
+});
+
+
+
+
 //Utility function to get Food
 async function getFood(req, res, next){
     let food;
     try{
         food = await Food.findById(req.params.id);
-        
+
         if(food == null){
             return res.status(404).json({message: "Food not found"});
         }
--- Final Project/Food Delivery/routes/users.js
+++ Final Project/Food Delivery/routes/users.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const User = require('../models/user');
 const router = express.Router();
+const jwt= require('jsonwebtoken');
+const e = require('express');
 
 //GET USERS
 //API GET /api/users
@@ -9,7 +11,7 @@ router.get('/', async (req, res) => {
         const users = await User.find();
         res.send(users);
     } catch (error) {
-        res.status(404).json({message: 'User not found'});
+        res.status(404).json({message: `No users currently available`});
     }
 });
 
@@ -22,7 +24,7 @@ router.get("/:id", getUser , (req, res) => {
         res.status(200).json(res.user);
     }
     else{
-        res.status(404).json({message: `User with ${req.params.id} not found`});
+        res.status(404).json({message: `Sorry user with ${req.params.id} not found`});
     }
 });
 
@@ -31,22 +33,49 @@ router.get("/:id", getUser , (req, res) => {
 //ADD USER
 //API POST /api/register
 router.post('/register', async (req, res) => {
-   const user = new User({
-       userName: req.body.userName,
-       email: req.body.email,
-       password: req.body.password,
-       address: req.body.address
-   });
 
-   try {
-       const newUser = await user.save();
-       res.status(200).json(newUser);
+   const checkIfUserAlreadyRegistered = User.findOne({userName: req.params.userName});
+   if(checkIfUserAlreadyRegistered == null){
+
+        const user = new User({
+        userName: req.body.userName,
+        email: req.body.email,
+        password: req.body.password,
+        address: req.body.address
+        });
+ 
+    try {
+        const newUser = await user.save();
+        res.status(200).json(newUser);
+    }
+    catch(error) {
+        res.status(400).json({message: error.message})
+    }
    }
-   catch(error) {
-       res.status(400).json({message: error.message})
-   }
-   
+   else{
+       res.status(409).json({message: `The userName ${req.body.userName} already exists. Please sign in`});
+   } 
 });
+
+
+//UPDATE THE USER
+//API POST /api/update/:id
+router.put('/:id', getUser, async (req, res) => {
+
+    res.user.userName= req.body.userName;
+    res.user.email = req.body.email;
+    res.user.password= req.body.password;
+    res.user.address= req.body.address;
+
+
+    try{
+        const updatedUser = await res.user.save();
+        res.status(200).json(updatedUser);
+    }
+    catch(error) {
+        res.status(404).json({message: `Sorry user with ${req.params.id} not found`});
+    }
+})
 
 
 
@@ -70,25 +99,62 @@ router.patch('/:id', getUser, async (req, res) => {
         res.status(200).json(updatedUser);
     }
     catch(error) {
-        res.status(404).json({message: `User with ${req.params.id} not found`});
+        res.status(404).json({message: `Sorry user with ${req.params.id} not found`});
     }
 })
 
 
 //DELETING USER
 //API DELETE /api/users/:id
-router.delete('/:id', getUser, async (req, res) => {
+router.delete('/:id',  getUser,  async (req, res) => {
     try{
         await res.user.remove();
         res.json({message:"User deleted successfully"})
     }
     catch(error) {
-        res.status(404).json({message: `User with ${req.params.id} not found`});
+        res.status(404).json({message: `Sorry user with ${req.params.id} not found`});
     }
-})
+});
 
+
+
+//VALIDATE IF USER IS PRESENT 
+//API POST /api/users/authenticate
+router.post('/authenticate', async (req, res)=>{
+    const {userName, password} = req.body;
+    const user = await User.findOne({userName: userName, password: password});
+    if(user != null){
+        jwt.sign({user: user}, 'secretKey', (err, token) => {
+            res.status(200).json({message: "User has been signed successfully", token: token});
+        });
+    }
+    else{
+        res.status(403).json({message:"User not found. Please check the credentials"});
+    }
+});
+
+
+//Middle ware function to verify token
+async function verifyToken(req, res, next){
+   const authHeader = req.headers['authorization'];
+   const auth = authHeader && authHeader.split(' ')[1];
+   if(auth == null){
+       //user has a token by thats not valid
+      return res.sendStatus(403);
+   }
+
+   jwt.verify(token, 'secretKey', (err, user) =>{
+       if(err){
+           return res.sendStatus(403);
+       }
+
+       req.user = user;
+       next();
+   })
+}
 
 //Utility function to get User 
+//Middle ware 
 async function getUser(req, res, next){
     let user;
     try{
@@ -99,24 +165,11 @@ async function getUser(req, res, next){
         }
     }
     catch(error){
-        return res.status(500).json({message:"Not fount"});
+        return res.status(500).json({message:"User not found"});
     }
     res.user = user;
     next();
 }
-
-//VALIDATE IF USER IS PRESENT 
-//API POST /api/users/authenticate
-router.post('/autheticate', async (req, res)=>{
-    const {userName, password} = req.body;
-    const user = await User.findOne({userName: userName, password: password});
-    if(user != null){
-        res.status(200).json({message:"User logged in successfully"});
-    }
-    else{
-        res.status(403).json({message:"User not found"});
-    }
-})
 
 
 module.exports = router;
