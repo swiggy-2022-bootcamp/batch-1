f0e2846aba4c125b3504676e0be50d1ec40d1695
Aaditya <aadityakhetan123@gmail.com>
2022-01-07 23:24:43 +0530

    boilerplate code added

---
+++ StackOverflowApp/index.js
@@ -0,0 +1,40 @@
+// packages
+import express from "express"
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 8080;
+
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+
+const connection = mongoose.connection
+
+connection.on("open", ()=>{
+    console.log("✅ Mongodb connected successfully");
+})
+
+connection.on("error", ()=> {
+    console.log("❌ Error while connecting mongodb")
+})
+
+connection.on("disconnected", () => {
+    console.log("❌ Mongdb disconnected");
+})
+
+app.get("/", (req, res)=>{
+    res.status(200).send({
+        "status": "Connected successfully",
+    })
+})
+
+app.listen(PORT, ()=>{
+    console.log(`✅ Server listening on PORT ${PORT}`)
+})
