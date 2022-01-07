f6f92b70233195d9b2ea880ed4c6801ce3dd7109
devansh5 <devkumar5436@gmail.com>
2022-01-07 19:26:31 +0530

    folder

--- Day1/index.js
+++
@@ -1,5 +0,0 @@
-function alert_page_loaded(){
-    alert('Dom is fully loaded now and we can have some fun.');
-}
-
-alert_page_loaded();
---
+++ devansh-chaubey/index.js
@@ -0,0 +1,4 @@
+alert('change the page color by clicking on button')
+function colorchange(){
+    document.body.style.backgroundColor='#0a0';
+}
