0e59e5781b107921c4a766d4a2013e458daedb0b
Ashwin G <47941624+ashwin2125@users.noreply.github.com>
2022-01-08 19:56:46 +0530

    improved project structure

---
+++ ashwin-gopalsamy/case-study-project/project-converge/src/alpha/v01/src/app/index.js
@@ -0,0 +1,4 @@
+var student17110013 = require('./my-modules');
+student17110013.name("Ashwin Gopalsamy");
+student17110013.age(22);
+student17110013.nationality("Indian");
---
+++ ashwin-gopalsamy/case-study-project/project-converge/src/alpha/v01/src/app/my-modules.js
@@ -0,0 +1,13 @@
+var summary = {
+    name: function (name) {
+        console.log("Name :"+name);
+    },
+    age: function(age){
+        console.log("Age :"+age);
+    },
+    nationality: function(nationality){
+        console.log("Nationality : "+nationality);
+    }
+};
+
+module.exports = summary;
