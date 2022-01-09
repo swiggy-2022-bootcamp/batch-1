64c5c0b81c633378cc058eea84d81c99112e21ff
Divyansh Dixit <ddivyansh18@gmail.com>
2022-01-09 18:23:27 +0530

    Refactored Order APIs

--- Food Ordering App Backend/app.js
+++ Food Ordering App Backend/app.js
@@ -23,6 +23,7 @@ const indexRouter = require('./routes/index.js');
 const restaurantsRouter = require('./routes/restaurant.js');
 const foodRouter = require('./routes/food.js');
 const cartsRouter = require('./routes/cart.js');
+const ordersRouter = require('./routes/orders.js');
 
 const connectDB = require('./db.js');
 const res = require('express/lib/response');
@@ -42,54 +43,8 @@ app.use('/api/food', foodRouter);
 
 app.use('/api/carts', auth, cartsRouter);
 
-async function saveOrderDetails(req) {
+app.use('/api/orders', auth, ordersRouter);
 
-    try {
-
-        console.log("USERID: ", req.params.userID);
-
-        const cartItems = await Cart.find({userID : req.params.userID});
-
-        if(!cartItems)
-        return res.send(409).json({"message" : "Cart is empty"});
-
-        await Cart.deleteMany({userID : req.params.userID});
-
-        const order = await Order.create({
-            userName : req.body.username,
-            userID : req.params.userID,
-            items : cartItems
-        });
-
-        await order.save();
-        return order;
-    
-    } catch(e) {
-        console.log(e.message);
-        return false;
-    }
-}
-
-
-app.post('/api/orders/:userID', auth, async(req, res) => {
-
-    const order = await saveOrderDetails(req);
-
-    if(order)
-    return res.status(200).json(order);
-
-    res.status(500).json({"message" : "Transaction Failed"});
-});
-
-app.get('/api/orders/:userID', auth, async(req, res) => {
-
-    const orders = await Order.find({userID : req.params.userID});
-
-    if(orders.length !== 0)
-    return res.status(200).json(orders);
-
-    res.status(200).json({"message" : "No orders for given user"});
-});
 
 const port = process.env.port || 3000;
 
