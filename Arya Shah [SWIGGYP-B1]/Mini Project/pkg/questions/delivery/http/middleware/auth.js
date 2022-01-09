4d0379b7e674d5ac2bdf5f9cb976ca47110f6308
Arya Shah <arya.shah82@nmims.edu.in>
2022-01-10 00:56:05 +0530

    Create auth.js

---
+++ Arya Shah [SWIGGYP-B1]/Mini Project/pkg/questions/delivery/http/middleware/auth.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken')
+
+const auth = async(req, res, next) => {
+    const token = req.header('token')
+    try {
+        const data = jwt.verify(token, '8hhg5f56g7t67');
+        if (!data) {
+            res.status(401).send({ error: 'Not authorized to access this resource' })
+        }
+        next();
+    } catch (error) {
+      res.status(400).json({
+         status: 'erorr',
+         message: 'Invalid Signature',
+         data: error
+     });
+    }
+}
+
+module.exports = auth
