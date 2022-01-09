bf07f0c21091286c56152bb3a7b084b5ea43c61b
Aditya Surana <to.adityasurana@gmail.com>
2022-01-10 00:44:07 +0530

    Create header.component.ts

---
+++ adityasurana-SWYCDPB107/foodapp/foodapp-frontend/src/app/header/header.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth/auth.service';
+import { HttpAPIRequestService } from '../HttpAPIRequest.service';
+
+@Component({
+	selector: 'app-header',
+	templateUrl: './header.component.html',
+	styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+	Token = {
+		secretToken : ''
+	}
+  username: string;
+
+	constructor(
+		private request: HttpAPIRequestService,
+		private authService: AuthService,
+	) { }
+
+	ngOnInit(){
+    this.Token.secretToken = localStorage.getItem('userData');
+    this.request.verifyUser(this.Token).subscribe((userDetail) => {
+      this.username = userDetail.username;
+    })
+	}
+
+	onLogout(){
+		this.authService.logout();
+	}
+
+}
