c0d5e9fc66fd38039bf4e179444393ca74fba3e2
Chahat Bhatia <chahatbhatia2014@gmail.com>
2022-01-09 20:55:16 +0530

    Added Nodemailer

--- foodapp/utils/welcomeEmail/welcomeEmail.js
+++ foodapp/utils/welcomeEmail/welcomeEmail.js
@@ -1,13 +1,10 @@
 const nodemailer = require("nodemailer");
 
-//const userTemplate=require('./mailcontact');
-// const generateOtp=require('../generateotp');
 function mailUser(mailId,userName) { 
   const welcomeMessage=`
 <h3>Welcome ${userName}!</h3>
 <p>Enjoy our Food Fest sale this weekend.</p>`;
-    // let generateOtpOrder=generateOtp.generateOrderOtp();
-   // userTemplate(mailId,Password).then(data=> {
+   
       var transporter = nodemailer.createTransport({
         service: 'gmail',
         auth: {
@@ -32,30 +29,3 @@ function mailUser(mailId,userName) {
 }
 module.exports=mailUser;
 
-
-
-
-// var nodemailer = require('nodemailer');
-
-// var transporter = nodemailer.createTransport({
-//   service: 'gmail',
-//   auth: {
-//     user: 'chahat14bhatia@gmail.com',
-//     pass: 'Chahat@123'
-//   }
-// });
-
-// var mailOptions = {
-//   from: 'youremail@gmail.com',
-//   to: 'myfriend@yahoo.com',
-//   subject: 'Sending Email using Node.js',
-//   html: welcomeMessage
-// };
-
-// transporter.sendMail(mailOptions, function(error, info){
-//   if (error) {
-//     console.log(error);
-//   } else {
-//     console.log('Email sent: ' + info.response);
-//   }
-// });
