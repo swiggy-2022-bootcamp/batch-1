ea961cf305840d472ba3e0204d33e3bcac446d4b
Divyansh Dixit <ddivyansh18@gmail.com>
2022-01-09 16:41:12 +0530

    Refactored users APIs

--- Food Ordering App Backend/app.js
+++ Food Ordering App Backend/app.js
@@ -6,13 +6,19 @@ const app = express();
 const mongoose = require('mongoose');
 require("dotenv").config();
 
+
+// Model Imports
 const User = require('./schemas/user.js');
 const Restaurant = require('./schemas/restaurant.js');
 const Food = require('./schemas/food.js');
 const Cart = require('./schemas/cart.js');
 const Order = require('./schemas/order.js');
 
-const auth = require("./auth.js");
+// Middleware Imports
+const auth = require("./middleware/auth.js");
+
+// Router Imports
+const usersRouter = require('./routes/users.js');
 
 const connectDB = require('./db.js');
 const res = require('express/lib/response');
@@ -115,61 +121,7 @@ app.post('/api/authenticate', async (req, res) => {
 
 });
 
-app.get('/api/users', async (req, res) => {
-
-    const users = await User.find();
-    return res.status(200).json(users);
-
-});
-
-
-app.get('/api/users/:userID', async (req, res) => {
-
-    try {
-        const user = await User.find({_id : req.params.userID});
-        return res.status(200).json(user);
-    } catch(e) {
-        return res.status(404).json({"message" : `User with id ${req.params.userID} not found`});
-    }
-    
-});
-
-app.put('/api/users', async (req, res) => {
-
-    try {
-        const user = await User.findOne({_id : req.body._id});
-
-        for(let property of Object.keys(req.body)) {
-            
-            if(property === '_id')
-            continue;
-
-            if(user[property])
-                user[property] = req.body[property];
-        }
-
-        await user.save();
-        return res.status(200).json(user);
-
-    } catch(e) {
-        console.log(e.message);
-        return res.status(404).json({"message" : `User with id ${req.body._id} not found`});
-    }
-    
-});
-
-app.delete('/api/users/:userID', async (req, res) => {
-
-    try {
-        await User.deleteOne({_id : req.params.userID});
-        return res.status(200).json({"message" : `User deleted successfully`});
-
-    } catch(e) {
-        console.log(e.message);
-        return res.status(500).json({"message" : `Error deleting user with id ${req.params.userID}`});
-    }    
-
-});
+app.use('/api/users', usersRouter);
 
 async function saveRestaurantDetails(req) {
 
---
+++ Food Ordering App Backend/controllers/indexController.js
@@ -0,0 +1,93 @@
+exports.saveUserDetails = async function saveUserDetails(req) {
+
+    try {
+
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        const jwtToken = jwt.sign(
+                { 
+                    username : req.body.username, 
+                    password : hashedPassword
+                },
+                process.env.TOKEN_KEY,
+                {
+                    expiresIn: "2h",
+                }
+        );
+    
+        const user = await User.create({
+            username : req.body.username,
+            password : hashedPassword,
+            email : req.body.email,
+            address : {
+                houseno : req.body.address.houseno,
+                street : req.body.address.street,
+                city : req.body.address.city,
+                state : req.body.address.state,
+                zip : req.body.address.zip
+            },
+            token : jwtToken
+        });
+
+        await user.save();
+        return user;
+    
+    } catch(e) {
+        console.log(e.message);
+        return false;
+    }
+}
+
+exports.registerUser = async (req, res) => {
+    
+    try {
+    const oldUser = await User.findOne({ email : req.body.email });
+    if (oldUser) {
+        return res.status(409).send("User already registered. Please Login");
+    }   
+    } catch(e) {
+        console.log(e.message);
+        return;
+    }
+
+    const userSaved = await saveUserDetails(req);
+
+    if(userSaved) {
+    return res.send(userSaved);   
+    }
+
+    res.status(400).send("User registration not successful");
+}
+
+
+exports.authenticateUser = async (req, res) => {
+
+    const {email , password} = req.body;
+
+    const user = await User.findOne({ email });
+
+    if(!user) {
+        return res.status(400).json({"message" : "User not found"});
+    }
+
+    if(await bcrypt.compare(password, user.password)) {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        const token = jwt.sign(
+                { 
+                    username : req.body.username, 
+                    password : hashedPassword
+                },
+                process.env.TOKEN_KEY,
+                {
+                    expiresIn: "2h",
+                }
+        );
+        
+        user.token = token;
+        await user.save();
+        return res.status(200).json({"message" : "Login Successful"});
+    }
+    else {
+        return res.status(403).send({"message" : "Wrong Credentials"})
+    }
+
+}
