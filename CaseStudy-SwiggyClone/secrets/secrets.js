5b1af6bfb97702224491bfd4540054a75487b52a
abhibenne <abhibenne@gmail.com>
2022-01-09 20:08:03 +0530

    removed db links

--- CaseStudy-SwiggyClone/secrets/secrets.js
+++ CaseStudy-SwiggyClone/secrets/secrets.js
@@ -1,4 +1,4 @@
 module.exports={
-    JWT_KEY : 'nsajn1wnjsqsn1q13ksndk',
-    db_link : 'mongodb+srv://abhibenne:abhibenne@cluster0.1txb7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+    JWT_KEY : 'JWT_KEY_HERE',
+    db_link : 'MONGO DB LINK HERE'
 };
