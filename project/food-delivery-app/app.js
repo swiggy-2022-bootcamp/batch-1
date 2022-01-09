eee7f91c758d0c8bfafec6071a12d031e3b2c689
Ayan Dutta <ayan59dutta@gmail.com>
2022-01-10 05:00:14 +0530

    Updated app.js to include routes and middleware

--- project/food-delivery-app/app.js
+++ project/food-delivery-app/app.js
@@ -1,20 +1,61 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const createError = require("http-errors");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require("./routes/index");
 
 var app = express();
 
-app.use(logger('dev'));
+mongoose
+    .connect("mongodb+srv://ayan-dutta-swiggy-ipp-f.xrzw8.mongodb.net/", {
+        dbName: "ipp-demo-food-delivery-app",
+        user: "ayan-dutta-ipp",
+        pass: "ayan-dutta-ipp",
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        console.log("Mongodb connected!");
+    });
+
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+// app.use('/users', usersRouter);
+
+/// catch 404 and forward to error handler
+// app.use(function (req, res, next) {
+//     console.log('In error');
+//     var err = new Error('Not Found');
+//     err.status = 404;
+//     next(err);
+// });
+
+//middleware: 404 handler and pass to error handler
+app.use((req, res, next) => {
+    next(createError(404, "Not found"));
+});
+
+//Error Handler
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.send({
+        error: {
+            status: err.status || 500,
+            message: err.message,
+        },
+    });
+});
+
+app.listen(3000, () => {
+    console.log("Listening at 3000");
+});
 
 module.exports = app;
