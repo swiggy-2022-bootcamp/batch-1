b702647ea968559d54d7c39ea805d87f2a585cf6
Aditya Surana <to.adityasurana@gmail.com>
2022-01-10 00:45:19 +0530

    Update header.component.ts

--- adityasurana-SWYCDPB107/foodapp/foodapp-frontend/src/app/header/header.component.ts
+++ adityasurana-SWYCDPB107/foodapp/foodapp-frontend/src/app/header/header.component.ts
@@ -5,7 +5,6 @@ import { HttpAPIRequestService } from '../HttpAPIRequest.service';
 @Component({
 	selector: 'app-header',
 	templateUrl: './header.component.html',
-	styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
 	Token = {
